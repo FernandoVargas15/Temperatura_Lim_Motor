43f39cd4b843053a268a4130ea93ef3f56499f50
FernandoVargas15 <luis.vargas89@unach.mx>
2024-10-31 13:24:56 -0600

    Agregar archivos de proyecto.

---
+++ LimTemperatura/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LimTemperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btndesconectar = new System.Windows.Forms.Button();
+            this.tbLimTemp = new System.Windows.Forms.TextBox();
+            this.lbTemp = new System.Windows.Forms.Label();
+            this.lbConection = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Limite de temperatura:";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnConectar.Location = new System.Drawing.Point(53, 282);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(186, 92);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btndesconectar
+            // 
+            this.btndesconectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndesconectar.ForeColor = System.Drawing.Color.Red;
+            this.btndesconectar.Location = new System.Drawing.Point(321, 282);
+            this.btndesconectar.Name = "btndesconectar";
+            this.btndesconectar.Size = new System.Drawing.Size(182, 92);
+            this.btndesconectar.TabIndex = 2;
+            this.btndesconectar.Text = "Desconectar";
+            this.btndesconectar.UseVisualStyleBackColor = true;
+            this.btndesconectar.Click += new System.EventHandler(this.btndesconectar_Click);
+            // 
+            // tbLimTemp
+            // 
+            this.tbLimTemp.Location = new System.Drawing.Point(93, 76);
+            this.tbLimTemp.Name = "tbLimTemp";
+            this.tbLimTemp.Size = new System.Drawing.Size(120, 22);
+            this.tbLimTemp.TabIndex = 3;
+            // 
+            // lbTemp
+            // 
+            this.lbTemp.AutoSize = true;
+            this.lbTemp.Location = new System.Drawing.Point(400, 107);
+            this.lbTemp.Name = "lbTemp";
+            this.lbTemp.Size = new System.Drawing.Size(14, 16);
+            this.lbTemp.TabIndex = 4;
+            this.lbTemp.Text = "0";
+            // 
+            // lbConection
+            // 
+            this.lbConection.AutoSize = true;
+            this.lbConection.Location = new System.Drawing.Point(103, 233);
+            this.lbConection.Name = "lbConection";
+            this.lbConection.Size = new System.Drawing.Size(85, 16);
+            this.lbConection.TabIndex = 5;
+            this.lbConection.Text = "Sin Conexion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(399, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Temperatura:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbConection);
+            this.Controls.Add(this.lbTemp);
+            this.Controls.Add(this.tbLimTemp);
+            this.Controls.Add(this.btndesconectar);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btndesconectar;
+        private System.Windows.Forms.TextBox tbLimTemp;
+        private System.Windows.Forms.Label lbTemp;
+        private System.Windows.Forms.Label lbConection;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
